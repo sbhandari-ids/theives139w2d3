4cd7495eab75e8b2086ae339b5b7c8ba180ae309
SB <sbhandari-ids@gmail.com>
2024-01-21 21:01:15 -0500

    Week2 Day3 Assignments in JupiterNote

---
+++ area_etc.py
@@ -0,0 +1,13 @@
+#this module calculates the area of a house
+import math
+
+def sqft_area(l,w):
+    area = l*w
+    return area
+
+#this module calculates the circumference of a circle
+
+def circum_circle(r):
+    c = 2*math.pi*r 
+    return c
+
